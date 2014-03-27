3994cb09e34c8f52116fa45f5e31cf7130d37f21
johannes hanika <hanika@kit.edu>
2014-03-27 18:14:08 +0100

    main: some global state for the viewer

--- src/main.c
+++ src/main.c
@@ -5,12 +5,34 @@
 #include <stdlib.h>
 
 
+// fastest to type while the right hand is on the mouse, copy/pasting the file name.
+// this is true on a dvorak keyboard, where these are on the left homerow, middle + index fingers:
 #define PROG_NAME "eu"
+
+
+typedef struct eu_t
+{
+  fileinput_conversion_t conv;
+}
+eu_t;
+
+// global state:
+eu_t eu;
+
+/*
+  void (*onKeyDown)(keycode_t);
+  void (*onKeyPressed)(keycode_t);
+  void (*onKeyUp)(keycode_t);
+  void (*onMouseButtonUp)(mouse_t);
+  void (*onMouseButtonDown)(mouse_t);
+  void (*onMouseMove)(mouse_t);
+  void (*onClose)();
+  */
 
 
 int main(int argc, char *arg[])
 {
-  const int wd = 512, ht = 512;
+  const int wd = 1024, ht = 576;
   display_t *display = display_open("viewer", wd, ht);
 
   fileinput_t file;
@@ -25,21 +47,23 @@ int main(int argc, char *arg[])
     exit(2);
   }
 
-  fileinput_conversion_t c;
-  c.roi.x = 0;
-  c.roi.y = 0;
-  c.roi.w = wd;
-  c.roi.h = ht;
-  c.roi.scale = 1.0f;
-  c.colorin = s_passthrough;
-  c.colorout = s_passthrough;
+  eu.conv.roi.x = 0;
+  eu.conv.roi.y = 0;
+  eu.conv.roi.w = wd;
+  eu.conv.roi.h = ht;
+  eu.conv.roi.scale = 1.0f;
+  eu.conv.colorin = s_passthrough;
+  eu.conv.colorout = s_passthrough;
 
   uint8_t *pixels = (uint8_t *)aligned_alloc(16, wd*ht*3);
-  fileinput_grab(&file, &c, pixels);
 
   while(1)
   {
+    // get user input, wait for it if need be.
     if(display_wait_event(display)) break;
+    // update buffer from out-of-core storage
+    fileinput_grab(&file, &eu.conv, pixels);
+    // show on screen
     display_update(display, pixels);
   }
 
